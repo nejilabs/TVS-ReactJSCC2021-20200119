85d0362b403c532d73dabdf257c01803580fe8c7
nellyXinwei <nelsonalbajr@yahoo.com>
2021-02-06 16:40:11 +0800

    @e # 37:46 - Events

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types'
 import Button from "./Button";
 
 const Header = ({ title }) => {
+  const eventClick = () => {
+    console.log("Clicked!")
+  }
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="btn1" />
-      <Button color="blue" text="btn2" />
-      <Button color="red" text="btn3" />
-      <Button />
+      <Button color="green" text="Add" onClick={eventClick} />
     </header>
   )
 }
