f9ace71af6dce01fd1510395094b869bf3b5ae19
nellyXinwei <nelsonalbajr@yahoo.com>
2021-02-06 16:11:47 +0800

    @s 34:17 - Button Component | @e # 30:42 - Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
 
-const Header = (props) => {
+const Header = ({ title }) => {
   return (
-    <header>
-      <h1>{props.title}</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <button className="btn">Add</button>
     </header>
   )
 }
