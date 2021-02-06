970ecf8cb40c6746b474bab5457a424fcd042182
nellyXinwei <nelsonalbajr@yahoo.com>
2021-02-06 15:57:14 +0800

    @s 30:42 - Styling |@e # 28:50 - PropTypes && # 28:50 - PropTypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+const Header = (props) => {
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{props.title}</h1>
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: "Default Header Title"
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
 
 export default Header
