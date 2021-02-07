7f4f90d1f36e4b4745adcc62a027d7511dc9ef82
nellyXinwei <nelsonalbajr@yahoo.com>
2021-02-08 04:27:45 +0800

    @e # 49:30 - Icons with react-icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from "react"
 import Header from "./components/Header"
+import Tasks from "./components/Tasks"
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "task1", day: "day1", reminder: false },
+    { id: 2, text: "task2", day: "day2", reminder: true },
+    { id: 3, text: "task3", day: "day3", reminder: false }
+  ])
+
   return (
     <div>
       <Header title="Task Tracker" />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
